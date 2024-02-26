857d8a79686672dbb97e347e5365ab3ce091cef5
Axel GMX <agmx183@gmail.com>
2024-02-26 17:16:46 +0100

    JS : Récupération des données et affichage dans la console

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+const recipesRecovery = recipes.map((recipeData) => {
+  return new Recipe(
+    recipeData.id,
+    recipeData.image,
+    recipeData.name,
+    recipeData.servings,
+    recipeData.ingredients,
+    recipeData.time,
+    recipeData.description,
+    recipeData.appliance,
+    recipeData.ustensils
+  );
+});
+
+console.log("------------------------");
+console.log(recipesRecovery);
+console.log("------------------------");
