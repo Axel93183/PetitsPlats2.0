42324ec2e30b25471eca37e066882f65214a4c26
Axel GMX <agmx183@gmail.com>
2024-03-14 16:07:11 +0100

    JS : Recherche avancée - inputs des selecteurs

--- scripts/templates/selectorsTemplate.js
+++ scripts/templates/selectorsTemplate.js
@@ -1,6 +1,9 @@
 const ingredientsSelector = document.getElementById("ingredients");
-const applianceSelector = document.getElementById("appliances");
+const ingredientSearchInput = document.getElementById("ingredients-choice");
+const appliancesSelector = document.getElementById("appliances");
+const applianceSearchInput = document.getElementById("appliances-choice");
 const ustensilsSelector = document.getElementById("ustensils");
+const ustensilSearchInput = document.getElementById("ustensils-choice");
 
 const recipesElements = recipes.map((recipeData) => {
   return new Recipe(
@@ -18,128 +21,144 @@ const recipesElements = recipes.map((recipeData) => {
 
 /* Ingredient Selector*/
 
-// Fonction pour récupérer tous les ingrédients des recettes sans doublons
 function getAllIngredients() {
-  // Crée un ensemble pour stocker tous les ingrédients sans doublons
   const allIngredients = new Set();
-  // Parcourt chaque recette
   recipesElements.forEach((recipe) => {
-    // Parcourt chaque ingrédient de la recette
     recipe.ingredients.forEach((ingredientData) => {
-      // Ajoute l'ingrédient à l'ensemble
       allIngredients.add(ingredientData.ingredient);
     });
   });
-  // Retourne l'ensemble de tous les ingrédients
   return allIngredients;
 }
 
-// Fonction pour filtrer les ingrédients en fonction du terme de recherche
 function filterIngredients(searchTerm, allIngredients) {
-  // Crée un ensemble pour stocker les ingrédients filtrés
   const filteredIngredients = new Set();
-  // Parcourt tous les ingrédients
   allIngredients.forEach((ingredient) => {
-    // Vérifie si l'ingrédient contient le terme de recherche (en ignorant la casse)
     if (ingredient.toLowerCase().includes(searchTerm.toLowerCase())) {
-      // Ajoute l'ingrédient filtré à l'ensemble
       filteredIngredients.add(ingredient);
     }
   });
-  // Retourne l'ensemble des ingrédients filtrés
   return filteredIngredients;
 }
 
-// Fonction pour mettre à jour la liste des ingrédients dans le DOM
 function updateIngredientList(ingredients) {
-  // Vide le contenu actuel de l'élément HTML
   ingredientsSelector.innerHTML = "";
-  // Parcourt tous les ingrédients à afficher
   ingredients.forEach((ingredient) => {
-    // Met la première lettre de l'ingrédient en majuscule et le reste en minuscules
     const capitalizedIngredient =
       ingredient.charAt(0).toUpperCase() + ingredient.slice(1).toLowerCase();
-    // Crée un élément de liste HTML pour l'ingrédient
     const ingredientToSelect = document.createElement("li");
-    // Définit le texte de l'élément de liste comme l'ingrédient formatté
     ingredientToSelect.textContent = capitalizedIngredient;
-    // Ajoute l'élément de liste à l'élément HTML parent
     ingredientsSelector.appendChild(ingredientToSelect);
   });
 }
-// Sélectionne l'élément input de recherche
-const searchInput = document.getElementById("ingredients-choice");
 
-// Fonction pour gérer l'événement de saisie dans l'input de recherche
-function handleSearchInput() {
-  // Récupère le terme de recherche entré par l'utilisateur
-  const searchTerm = searchInput.value;
-  // Obtient tous les ingrédients disponibles
+updateIngredientList(getAllIngredients());
+
+// Ingredient Search Function
+function handleIngredientSearchInput() {
+  const searchTerm = ingredientSearchInput.value;
   const allIngredients = getAllIngredients();
-  // Filtre les ingrédients en fonction du terme de recherche
   const filteredIngredients = filterIngredients(searchTerm, allIngredients);
-  // Met à jour la liste des ingrédients affichés dans le DOM
   updateIngredientList(filteredIngredients);
 }
 
-// Écoute les événements de saisie dans l'input de recherche et appelle handleSearchInput()
-searchInput.addEventListener("input", handleSearchInput);
-
-// Appel initial pour afficher tous les ingrédients sans doublons au chargement de la page
-updateIngredientList(getAllIngredients());
+// Ingredient Input Event
+ingredientSearchInput.addEventListener("input", handleIngredientSearchInput);
 
 /* Appliance Selector */
-recipesElements.forEach((recipe) => {
-  const appliance = recipe.appliance;
 
-  const applianceToSelect = document.createElement("li");
-  applianceToSelect.textContent = appliance;
+function getAllAppliances() {
+  const allAppliances = new Set();
+  recipesElements.forEach((recipe) => {
+    const appliance = recipe.appliance;
+    allAppliances.add(appliance);
+  });
+  return allAppliances;
+}
 
-  const existingOptions = applianceSelector.querySelectorAll("li");
-
-  let optionExists = false;
-
-  existingOptions.forEach((option) => {
-    if (option.textContent === appliance) {
-      optionExists = true;
-      return;
+function filterAppliances(searchTerm, allAppliances) {
+  const filteredAppliances = new Set();
+  allAppliances.forEach((appliance) => {
+    if (appliance.toLowerCase().includes(searchTerm.toLowerCase())) {
+      filteredAppliances.add(appliance);
     }
   });
+  return filteredAppliances;
+}
 
-  if (!optionExists) {
-    applianceSelector.appendChild(applianceToSelect);
-  }
-});
+function updateApplianceList(appliances) {
+  appliancesSelector.innerHTML = "";
+  appliances.forEach((appliance) => {
+    const capitalizedAppliance =
+      appliance.charAt(0).toUpperCase() + appliance.slice(1).toLowerCase();
+    const applianceToSelect = document.createElement("li");
+    applianceToSelect.textContent = capitalizedAppliance;
+    appliancesSelector.appendChild(applianceToSelect);
+  });
+}
+
+updateApplianceList(getAllAppliances());
+
+// Appliance Search Function
+function handleApplianceSearchInput() {
+  const searchTerm = applianceSearchInput.value;
+  const allAppliances = getAllAppliances();
+  const filteredAppliances = filterAppliances(searchTerm, allAppliances);
+  updateApplianceList(filteredAppliances);
+}
+
+// Appliance Input Event
+applianceSearchInput.addEventListener("input", handleApplianceSearchInput);
 
 /* Ustensils Selector */
-recipesElements.forEach((recipe) => {
-  const ustensils = recipe.ustensils;
 
+function getAllUstensils() {
+  const allUstensils = new Set();
+  recipesElements.forEach((recipe) => {
+    recipe.ustensils.forEach((ustensilData) => {
+      allUstensils.add(ustensilData.toLowerCase());
+    });
+  });
+  return allUstensils;
+}
+
+function filterUstensils(searchTerm, allUstensils) {
+  const filteredUstensils = new Set();
+  allUstensils.forEach((ustensil) => {
+    if (ustensil.toLowerCase().includes(searchTerm.toLowerCase())) {
+      filteredUstensils.add(ustensil);
+    }
+  });
+  return filteredUstensils;
+}
+
+function updateUstensilList(ustensils) {
+  ustensilsSelector.innerHTML = "";
   ustensils.forEach((ustensil) => {
     const capitalizedUstensil =
       ustensil.charAt(0).toUpperCase() + ustensil.slice(1);
 
     const ustensilToSelect = document.createElement("li");
     ustensilToSelect.textContent = capitalizedUstensil;
-
-    const existingOptions = ustensilsSelector.querySelectorAll("li");
-
-    let optionExists = false;
-
-    existingOptions.forEach((option) => {
-      if (option.textContent === capitalizedUstensil) {
-        optionExists = true;
-        return;
-      }
-    });
-
-    if (!optionExists) {
-      ustensilsSelector.appendChild(ustensilToSelect);
-    }
+    ustensilsSelector.appendChild(ustensilToSelect);
   });
-});
+}
+
+updateUstensilList(getAllUstensils());
+
+// Ustensils Search Function
+function handleUstensilSearchInput() {
+  const searchTerm = ustensilSearchInput.value;
+  const allUstensils = getAllUstensils();
+  const filteredUstensils = filterUstensils(searchTerm, allUstensils);
+  updateUstensilList(filteredUstensils);
+}
+
+// Ustensils Input Event
+ustensilSearchInput.addEventListener("input", handleUstensilSearchInput);
 
 /* Search Tags */
+
 const searchDiv = document.getElementById("search-div");
 const searchSelectorsDiv = document.querySelector(".search-selectors");
 const searchTagsDiv = document.getElementById("search-tags");
@@ -185,6 +204,7 @@ SelectElementsUL.forEach((selectElement) => {
 });
 
 /* Display Selector */
+
 const selectors = document.querySelectorAll(".selector");
 
 selectors.forEach((selector) => {
