8317dd08f1e44f14303a7b3d2745710a6987c979
Axel GMX <agmx183@gmail.com>
2024-03-23 13:51:36 +0100

    Recherche avancée : Filtres par Tags selon type du sélecteurs (ingrédients, ustensiles, appareils)

--- scripts/utils/mainSearchRecipes.js
+++ scripts/utils/mainSearchRecipes.js
@@ -22,25 +22,62 @@ for (let i = 0; i < recipes.length; i++) {
   );
 }
 
-// Global Filter Function
-function filterRecipesByTerm(recipes, term) {
+// Filter by Query Function
+function filterRecipesByQuery(recipes, query) {
   const filtered = [];
   for (let i = 0; i < recipes.length; i++) {
     const recipe = recipes[i];
     if (
-      recipe.name.toLowerCase().includes(term) ||
-      recipe.description.toLowerCase().includes(term) ||
-      recipe.appliance.toLowerCase().includes(term) ||
+      recipe.name.toLowerCase().includes(query) ||
+      recipe.description.toLowerCase().includes(query) ||
+      recipe.appliance.toLowerCase().includes(query) ||
       recipe.ustensils.some((ustensil) =>
-        ustensil.toLowerCase().includes(term)
+        ustensil.toLowerCase().includes(query)
       ) ||
       recipe.ingredients.some((ingredient) =>
-        ingredient.ingredient.toLowerCase().includes(term)
+        ingredient.ingredient.toLowerCase().includes(query)
       )
     ) {
       filtered.push(recipe);
     }
   }
+  return filtered;
+}
+
+// Filter by Tag Name Function
+function filterRecipesByTag(recipes, tag) {
+  const filtered = [];
+  const tagName = tag.name.toLowerCase();
+
+  for (let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+
+    if (
+      tag.type === "ingredients" &&
+      recipe.ingredients.some((ingredient) =>
+        ingredient.ingredient.toLowerCase().includes(tagName)
+      )
+    ) {
+      filtered.push(recipe);
+    }
+
+    if (
+      tag.type === "appliances" &&
+      recipe.appliance.toLowerCase().includes(tagName)
+    ) {
+      filtered.push(recipe);
+    }
+
+    if (
+      tag.type === "ustensils" &&
+      recipe.ustensils.some((ustensil) =>
+        ustensil.toLowerCase().includes(tagName)
+      )
+    ) {
+      filtered.push(recipe);
+    }
+  }
+
   return filtered;
 }
 
@@ -51,13 +88,13 @@ function filterRecipes() {
   // Filter by Tag
   for (let i = 0; i < selectedTags.length; i++) {
     const tag = selectedTags[i];
-    filteredRecipes = filterRecipesByTerm(filteredRecipes, tag);
+    filteredRecipes = filterRecipesByTag(filteredRecipes, tag);
   }
 
   // Filter by Query
   const query = mainSearchInput.value.trim().toLowerCase();
   if (query.length >= 3) {
-    filteredRecipes = filterRecipesByTerm(filteredRecipes, query);
+    filteredRecipes = filterRecipesByQuery(filteredRecipes, query);
   }
 
   return filteredRecipes;
@@ -106,8 +143,12 @@ let selectedTags = [];
 // Search by Tag Function
 function searchByTag(event) {
   const tag = event.target.textContent.trim().toLowerCase();
+  const type = event.target.parentNode.id.trim().toLowerCase();
   if (!selectedTags.includes(tag)) {
-    selectedTags.push(tag);
+    selectedTags.push({
+      type: type,
+      name: tag,
+    });
   }
   const filteredRecipes = filterRecipes();
   displayRecipesResults(filteredRecipes);
@@ -115,7 +156,7 @@ function searchByTag(event) {
 
 // Remove Tag Function
 function removeTag(tag) {
-  selectedTags = selectedTags.filter((selectedTag) => selectedTag !== tag);
+  selectedTags = selectedTags.filter((selectedTag) => selectedTag.name !== tag);
   const filteredRecipes = filterRecipes();
   displayRecipesResults(filteredRecipes);
 }
